933e9990a20f20619364c020da116407fc4d4818
tanymanera <gaetanomanera@outlook.it>
2020-06-10 20:26:38 +0200

    added app.secret_key

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+app.secret_key = "super secret key"
 
 @app.route("/")
 def index():
